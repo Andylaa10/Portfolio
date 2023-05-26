aa4a5c0deff3d1e9092d8da4de1669164fa356e0
Andylaa10 <andylaa100@gmail.com>
2023-05-26 15:28:59 +0200

    style stepper in progress

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {NgOptimizedImage} from "@angular/common";
+import {MatStepperModule} from "@angular/material/stepper";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatSidenavModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    NgOptimizedImage,
+    MatStepperModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
